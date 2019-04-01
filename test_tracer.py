37d449e6e9a705b1b5108dfdd7647e9a3717e153
Marine Lasbleis <marine.lasbleis@gmail.com>
2019-04-01 08:23:14 +0200

    update test tracers for run

--- test_tracer.py
+++ test_tracer.py
@@ -6,5 +6,28 @@ import numpy as np
 if __name__ == "__main__":
 
 
-    model = geodyn_analytical_flows.Yoshida96(0.)
-    tracers.Swarm(30, model, model.tau_ic/200)
+    model = geodyn_analytical_flows.Yoshida96(0.00)
+    tracers.Swarm(50, model, model.tau_ic/200, "Yoshida/test_tracers_000")
+
+    model = geodyn_analytical_flows.Yoshida96(0.00)
+    tracers.Swarm(50, model, model.tau_ic/1000, "Yoshida/test_tracers_000_1000pts")
+
+    model = geodyn_analytical_flows.Yoshida96(0.10)
+    tracers.Swarm(50, model, model.tau_ic/200, "Yoshida/test_tracers_010")
+
+    model = geodyn_analytical_flows.Yoshida96(0.20)
+    tracers.Swarm(50, model, model.tau_ic/200, "Yoshida/test_tracers_020")
+
+
+    model = geodyn_analytical_flows.Yoshida96(0.05)
+    tracers.Swarm(50, model, model.tau_ic/200, "Yoshida/test_tracers_005")
+
+    model = geodyn_analytical_flows.Yoshida96(0.15)
+    tracers.Swarm(50, model, model.tau_ic/200, "Yoshida/test_tracers_015")
+
+    model = geodyn_analytical_flows.Yoshida96(0.25)
+    tracers.Swarm(50, model, model.tau_ic/200, "Yoshida/test_tracers_025")
+
+
+    model = geodyn_analytical_flows.Yoshida96(0.30)
+    tracers.Swarm(50, model, model.tau_ic/200, "Yoshida/test_tracers_030")
